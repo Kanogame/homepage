7b15e60d04b8ec2b454eabf9f39ebde36b8e5236
kanogame <wasdqert1222@gmail.com>
2023-11-03 22:46:04 +0700

    0.1 deployable version done!

--- nginx/src/server.js
+++ nginx/src/server.js
@@ -4,17 +4,17 @@ const daily = [{
 },
 {
   name: "solve 1 task on genki",
-  buttonId: "duolingo",
+  buttonId: "genki",
 }, {
   name: "solve 1 CTF on picoCTF",
-  buttonId: "duolingo",
+  buttonId: "picoCTF",
 }]
 
 const dailyContainer = document.querySelector(".daily");
 for (i of daily) {
   const task = document.createElement("div");
   const status = await GetTaskStatus(i.buttonId);
-  task.innerHTML = (status.value === "1" ? "good" : "bad") + i.name; 
+  task.innerHTML =(status.value === "1" ? "<span style='color:#9EBA96'>\uf42e</span> " : "<span style='color:#f9cb74'>!</span> ") + i.name; 
   document.getElementById(i.buttonId).addEventListener("click", () => {
     return SetTaskStatus(i.buttonId);
   });
