f2a4a6d3fa3a0ca650d4feb6c549920df3f00724
kanogame <wasdqert1222@gmail.com>
2023-12-27 17:55:50 +0700

    genetate links form json config

--- nginx/src/config.js
+++ nginx/src/config.js
@@ -1,10 +1,35 @@
 import data from "../config/links.json" assert {type: 'json'};
+const tabList = data.tabs;
+
+const tabsEl = document.createElement("div");
+tabsEl.classList.add("activities");
+tabsEl.style.display = "none";
+
+for (const tab of tabList) {
+    const tabEl = createNewTab(tab.name);
+    for (const link of tab.links) {
+        const linkEl = createNewLink(link.name, link.link);
+        tabEl.appendChild(linkEl); 
+    }
+    tabsEl.appendChild(tabEl);
+}
+
+document.getElementById("activ").appendChild(tabsEl);
+
+function createNewLink(linkText, link) {
+    const linkEl = document.createElement("a");
+    linkEl.classList.add("el");
+    linkEl.href = link; //hope no xss
+    linkEl.appendChild(document.createTextNode(linkText));
+    return linkEl
+}
 
 function createNewTab(tabName) {
     const tab = document.createElement("div");
     const tabText = document.createElement("div");
     tabText.classList.add("path");
     tab.classList.add("section");
-    tab.appendChild(tabText.appendChild(document.createTextNode(tabName)));
+    tabText.appendChild(document.createTextNode(tabName));
+    tab.appendChild(tabText);
     return tab
 }
